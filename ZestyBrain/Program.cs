2efd388060501e5118a6f8037366e60f6f630b52
Alexs <alexis1998duvan@gmail.com>
2017-07-10 02:04:50 -0600

    modo dificilp

--- ZestyBrain/Program.cs
+++ ZestyBrain/Program.cs
@@ -16,7 +16,7 @@ namespace ZestyBrain
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PruebaCuerpoHumano());
+            Application.Run(new FrmPortada());
         }
     }
 }
